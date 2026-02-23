ba5516dfe57ed40fad0dd7a817b959412064cdb0
SaraJB07 <sarbarrera@uniboyaca.edu.co>
2026-02-23 15:59:22 -0500

    Cambios de clase

---
+++ src/models/Pokemon.js
@@ -0,0 +1,10 @@
+
+export default class Pokemon {
+    constructor(id, name, types, sprite) {
+        this.id = id;
+        this.name = name;
+        this.types = types;
+        this.sprite = sprite;
+
+    }
+}
